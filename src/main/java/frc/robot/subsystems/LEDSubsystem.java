a523258a2956efb71b7419daf7bd54b037a9fa7d
Garrett Jordan <garrettrjordan@gmail.com>
2026-02-26 21:53:13 -0800

    Modified LED Patterns

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -11,6 +11,7 @@ import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -24,17 +25,17 @@ public class LEDSubsystem extends SubsystemBase{
     private LEDPattern pattern;
 
     //max brightness
-    private final LEDPattern rainbowTemplate = LEDPattern.rainbow(255, 64);
+    private final LEDPattern rainbowTemplate = LEDPattern.rainbow(255, RobotBase.isReal() ? 64 : 255);
     private final LEDPattern gradTemplate = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, Color.kCyan, Color.kBlack);
 
-    //14 LEDs per meter 
+    // 14 LEDs per meter 
     private static final Distance kLedSpacing = Meters.of(1 / 14.0);
     
     private Boolean hasLock = false;
     private Boolean isShooting = false;
 
-    private final LEDPattern ScrollingRainbow = rainbowTemplate.scrollAtAbsoluteSpeed(MetersPerSecond.of(10), kLedSpacing);
-    private final LEDPattern ScrollingGrad = gradTemplate.scrollAtAbsoluteSpeed(MetersPerSecond.of(0.5), kLedSpacing);
+    private final LEDPattern ScrollingRainbow = rainbowTemplate.scrollAtAbsoluteSpeed(MetersPerSecond.of(1.0), kLedSpacing);
+    private final LEDPattern ScrollingGrad = gradTemplate.scrollAtAbsoluteSpeed(MetersPerSecond.of(4), kLedSpacing);
 
     // private final LEDPattern BreatheRainbow = rainbowTemplate.breathe(Seconds.of());
     //private final LEDPattern BreatheGrad = gradTemplate.breathe(Seconds.of(0.5));
